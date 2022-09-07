053e6616d6834800f457fb7435a92eef9733c8ce
morisitakanta <morisitakanta@gmail.com>
2022-09-07 19:05:58 +0900

    add pubs, subs and callbacks

--- include/local_goal_creator/local_goal_creator.h
+++ include/local_goal_creator/local_goal_creator.h
@@ -2,6 +2,11 @@
 #define __LOCAL_GOAL_CREATOR_H__
 
 #include <ros/ros.h>
+#include <std_msgs/Int32MultiArray.h>
+#include "amsl_navigation_msgs/NodeEdgeMap.h"
+#include "amsl_navigation_msgs/Node.h"
+#include "amsl_navigation_msgs/Edge.h"
+#include <geometry_msgs/PoseStamped.h>
 
 class LocalGoalCreator
 {
@@ -10,10 +15,25 @@ class LocalGoalCreator
         void process();
 
     private:
+        // callbacks
+        void checkpoint_callback(const std_msgs::Int32MultiArray::ConstPtr& msg);
+        void node_edge_callback(const amsl_navigation_msgs::NodeEdgeMap::ConstPtr& msg);
+        void current_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+
+        // other functions
+
+        // private params
         int hz_;
 
+        // other params
+
+        // ros
         ros::NodeHandle nh_;
         ros::NodeHandle private_nh_;
+        ros::Subscriber checkpoint_sub_;
+        ros::Subscriber node_edge_sub_;
+        ros::Subscriber current_pose_sub_;
+        ros::Publisher local_goal_pub_;
 };
 
 #endif // __LOCAL_GOAL_CREATOR_H__
--- src/local_goal_creator.cpp
+++ src/local_goal_creator.cpp
@@ -5,6 +5,26 @@ LocalGoalCreator::LocalGoalCreator():
     private_nh_("~")
 {
     private_nh_.param("hz", hz_, 10);
+
+    checkpoint_sub_ = nh_.subscribe("/checkpoint", 1, &LocalGoalCreator::checkpoint_callback, this);
+    node_edge_sub_ = nh_.subscribe("/node_edge_map", 1, &LocalGoalCreator::node_edge_callback, this);
+    current_pose_sub_ = nh_.subscribe("/current_pose", 1, &LocalGoalCreator::current_pose_callback, this);
+    local_goal_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("/local_goal", 1);
+}
+
+void LocalGoalCreator::checkpoint_callback(const std_msgs::Int32MultiArray::ConstPtr& msg)
+{
+    ROS_INFO("checkpoint_callback");
+}
+
+void LocalGoalCreator::node_edge_callback(const amsl_navigation_msgs::NodeEdgeMap::ConstPtr& msg)
+{
+    ROS_INFO("node_edge_callback");
+}
+
+void LocalGoalCreator::current_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& msg)
+{
+    ROS_INFO("current_pose_callback");
 }
 
 void LocalGoalCreator::process()
