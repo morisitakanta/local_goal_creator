ba4519234102f62277b8b3aff305c7e1b02129f1
morisitakanta <morisitakanta@gmail.com>
2022-09-10 16:33:30 +0900

    first success (frame_id: "map")

--- include/local_goal_creator/local_goal_creator.h
+++ include/local_goal_creator/local_goal_creator.h
@@ -7,6 +7,9 @@
 #include "amsl_navigation_msgs/Node.h"
 #include "amsl_navigation_msgs/Edge.h"
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <tf/transform_datatypes.h>
 
 class LocalGoalCreator
 {
@@ -18,14 +21,34 @@ class LocalGoalCreator
         // callbacks
         void checkpoint_callback(const std_msgs::Int32MultiArray::ConstPtr& msg);
         void node_edge_callback(const amsl_navigation_msgs::NodeEdgeMap::ConstPtr& msg);
-        void current_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+        void current_pose_callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg);
 
         // other functions
+        void get_node2node_poses(int node0_id, int node1_id, std::vector<geometry_msgs::PoseStamped>& node2node_poses);
+        bool reached_checkpoint(int next_checkpoint_id, geometry_msgs::PoseStamped current_pose);
+        geometry_msgs::PoseStamped get_local_goal(std::vector<geometry_msgs::PoseStamped> &node2node_poses, int &poses_index, geometry_msgs::PoseStamped current_pose);
+        bool reached_goal(int goal_node_id, geometry_msgs::PoseStamped current_pose);
 
         // private params
         int hz_;
+        int start_node_;
+        int goal_node_;
+        double local_goal_interval_;
+        double pass_through_radius_;
 
         // other params
+        bool checkpoint_received_;
+        bool node_edge_map_received_;
+        bool current_pose_updated_;
+        std_msgs::Int32MultiArray checkpoint_;
+        amsl_navigation_msgs::NodeEdgeMap node_edge_map_;
+        geometry_msgs::PoseStamped current_pose_;
+        int current_checkpoint_id_;
+        int next_checkpoint_id_;
+        std::vector<int> node_id_list_;
+        std::vector<geometry_msgs::PoseStamped> local_goal_poses_;
+        int local_goal_index_;
+        geometry_msgs::PoseStamped local_goal_;
 
         // ros
         ros::NodeHandle nh_;
--- src/local_goal_creator.cpp
+++ src/local_goal_creator.cpp
@@ -1,30 +1,171 @@
 #include "local_goal_creator/local_goal_creator.h"
 
-LocalGoalCreator::LocalGoalCreator():
-    nh_(),
-    private_nh_("~")
+LocalGoalCreator::LocalGoalCreator() : nh_(),
+                                       private_nh_("~")
 {
     private_nh_.param("hz", hz_, 10);
+    private_nh_.param("start_node", start_node_, 0);
+    private_nh_.param("goal_node", goal_node_, 1);
+    private_nh_.param("local_goal_interval", local_goal_interval_, 1.0);
+    private_nh_.param("pass_through_radius", pass_through_radius_, 5.0);
 
     checkpoint_sub_ = nh_.subscribe("/checkpoint", 1, &LocalGoalCreator::checkpoint_callback, this);
     node_edge_sub_ = nh_.subscribe("/node_edge_map", 1, &LocalGoalCreator::node_edge_callback, this);
     current_pose_sub_ = nh_.subscribe("/current_pose", 1, &LocalGoalCreator::current_pose_callback, this);
     local_goal_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("/local_goal", 1);
+
+    checkpoint_received_ = false;
+    node_edge_map_received_ = false;
+    current_pose_updated_ = false;
+    current_checkpoint_id_ = start_node_;
+    local_goal_index_ = 0;
 }
 
-void LocalGoalCreator::checkpoint_callback(const std_msgs::Int32MultiArray::ConstPtr& msg)
+void LocalGoalCreator::checkpoint_callback(const std_msgs::Int32MultiArray::ConstPtr &msg)
 {
-    ROS_INFO("checkpoint_callback");
+    // ROS_INFO("checkpoint_callback");
+    if (checkpoint_received_ == false)
+    {
+        checkpoint_ = *msg;
+        if (checkpoint_.data.size() > 2)
+        {
+            checkpoint_received_ = true;
+            next_checkpoint_id_ = checkpoint_.data[0];
+            // if (next_checkpoint_id_ == start_node_)
+            //     next_checkpoint_id_ = checkpoint_.data[1];
+            ROS_INFO("checkpoint size: %d", (int)checkpoint_.data.size());
+            while (current_checkpoint_id_ == next_checkpoint_id_)
+            {
+                checkpoint_.data.erase(checkpoint_.data.begin());
+                next_checkpoint_id_ = checkpoint_.data[0];
+                ROS_INFO("current_checkpoint_id_ : %d", current_checkpoint_id_);
+                ROS_INFO("next_checkpoint_id_ : %d", next_checkpoint_id_);
+                ROS_INFO("checkpoint_.data.size() : %d", (int)checkpoint_.data.size());
+            }
+        }
+    }
 }
 
-void LocalGoalCreator::node_edge_callback(const amsl_navigation_msgs::NodeEdgeMap::ConstPtr& msg)
+void LocalGoalCreator::node_edge_callback(const amsl_navigation_msgs::NodeEdgeMap::ConstPtr &msg)
 {
-    ROS_INFO("node_edge_callback");
+    // ROS_INFO("node_edge_callback");
+    if (node_edge_map_received_ == false)
+    {
+        node_edge_map_ = *msg;
+        if (node_edge_map_.nodes.size() != 0 && node_edge_map_.edges.size() != 0)
+        {
+            node_edge_map_received_ = true;
+            for (auto &node : node_edge_map_.nodes)
+                node_id_list_.push_back(node.id);
+        }
+    }
 }
 
-void LocalGoalCreator::current_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& msg)
+void LocalGoalCreator::current_pose_callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg)
 {
-    ROS_INFO("current_pose_callback");
+    // ROS_INFO("current_pose_callback");
+    if (current_pose_updated_ == false)
+    {
+        current_pose_.header = msg->header;
+        current_pose_.pose = msg->pose.pose;
+        current_pose_updated_ = true;
+    }
+}
+// {
+//     // ROS_INFO("current_pose_callback");
+//     current_pose_ = *msg;
+//     current_pose_updated_ = true;
+// }
+
+void LocalGoalCreator::get_node2node_poses(int node0_id, int node1_id, std::vector<geometry_msgs::PoseStamped> &node2node_poses)
+{
+    ROS_INFO("------------------------------------");
+    ROS_INFO("get_node2node_poses");
+    int node0_idx = find(node_id_list_.begin(), node_id_list_.end(), node0_id) - node_id_list_.begin();
+    int node1_idx = find(node_id_list_.begin(), node_id_list_.end(), node1_id) - node_id_list_.begin();
+    geometry_msgs::Point node0_pos = node_edge_map_.nodes[node0_idx].point;
+    geometry_msgs::Point node1_pos = node_edge_map_.nodes[node1_idx].point;
+    node2node_poses.clear();
+    double direction = atan2(node1_pos.y - node0_pos.y, node1_pos.x - node0_pos.x);
+
+    ROS_INFO("node0_id: %d (%f, %f)", node0_id, node0_pos.x, node0_pos.y);
+    ROS_INFO("node1_id: %d (%f, %f)", node1_id, node1_pos.x, node1_pos.y);
+    ROS_INFO("direction: %f", direction);
+
+    while (true)
+    {
+        geometry_msgs::PoseStamped node2node_pose;
+        node2node_pose.header.frame_id = "map";
+        node2node_pose.pose.position.x = node0_pos.x + local_goal_interval_ * cos(direction);
+        node2node_pose.pose.position.y = node0_pos.y + local_goal_interval_ * sin(direction);
+        node2node_pose.pose.position.z = 0;
+        node2node_pose.pose.orientation = tf::createQuaternionMsgFromYaw(direction);
+        node2node_poses.push_back(node2node_pose);
+
+        ROS_INFO("node2node_pose: %f, %f", node2node_pose.pose.position.x, node2node_pose.pose.position.y);
+
+        if(node2node_poses.size() > 1e6)
+            break;
+
+        if (sqrt(pow(node2node_pose.pose.position.x - node1_pos.x, 2) + pow(node2node_pose.pose.position.y - node1_pos.y, 2)) < local_goal_interval_)
+            break;
+        node0_pos = node2node_pose.pose.position;
+    }
+    // ROS_INFO("------------------------------------");
+}
+
+bool LocalGoalCreator::reached_checkpoint(int next_checkpoint_id, geometry_msgs::PoseStamped current_pose)
+{
+    // ROS_INFO("------------------------------------");
+    // ROS_INFO("reached_checkpoint");
+    int checkpoint_idx = find(node_id_list_.begin(), node_id_list_.end(), next_checkpoint_id) - node_id_list_.begin();
+    double checkpoint_x = node_edge_map_.nodes[checkpoint_idx].point.x;
+    double checkpoint_y = node_edge_map_.nodes[checkpoint_idx].point.y;
+
+    // ROS_INFO("next_checkpoint_id: %d", next_checkpoint_id);
+    // ROS_INFO("checkpoint_x: %f checkpoint_y: %f", checkpoint_x, checkpoint_y);
+    // ROS_INFO("current_pose_x: %f current_pose_y: %f", current_pose.pose.position.x, current_pose.pose.position.y);
+    // ROS_INFO("------------------------------------");
+
+    if (sqrt(pow(current_pose.pose.position.x - checkpoint_x, 2) + pow(current_pose.pose.position.y - checkpoint_y, 2)) < pass_through_radius_)
+        return true;
+    else
+        return false;
+}
+
+geometry_msgs::PoseStamped LocalGoalCreator::get_local_goal(std::vector<geometry_msgs::PoseStamped> &node2node_poses, int &poses_index, geometry_msgs::PoseStamped current_pose)
+{
+    // ROS_INFO("get_local_goal");
+    double current_local_goal_x = local_goal_.pose.position.x;
+    double current_local_goal_y = local_goal_.pose.position.y;
+    if (sqrt(pow(current_pose.pose.position.x - current_local_goal_x, 2) + pow(current_pose.pose.position.y - current_local_goal_y, 2)) < pass_through_radius_)
+    {
+        poses_index++;
+        if (poses_index >= node2node_poses.size())
+            poses_index = node2node_poses.size() - 1;
+    }
+    geometry_msgs::PoseStamped local_goal = node2node_poses[poses_index];
+    local_goal.header.frame_id = "map";
+    return local_goal;
+}
+
+bool LocalGoalCreator::reached_goal(int goal_node_id, geometry_msgs::PoseStamped current_pose)
+{
+    // ROS_INFO("------------------------------------");
+    // ROS_INFO("reached_goal");
+    int goal_idx = find(node_id_list_.begin(), node_id_list_.end(), goal_node_id) - node_id_list_.begin();
+    double goal_x = node_edge_map_.nodes[goal_idx].point.x;
+    double goal_y = node_edge_map_.nodes[goal_idx].point.y;
+
+    // ROS_INFO("goal_node_id: %d", goal_node_id);
+    // ROS_INFO("goal_x: %f goal_y: %f", goal_x, goal_y);
+    // ROS_INFO("current_pose_x: %f current_pose_y: %f", current_pose.pose.position.x, current_pose.pose.position.y);
+    // ROS_INFO("------------------------------------");
+
+    if (sqrt(pow(current_pose.pose.position.x - goal_x, 2) + pow(current_pose.pose.position.y - goal_y, 2)) < pass_through_radius_)
+        return true;
+    else
+        return false;
 }
 
 void LocalGoalCreator::process()
@@ -32,6 +173,57 @@ void LocalGoalCreator::process()
     ros::Rate rate(hz_);
     while (ros::ok())
     {
+        if (checkpoint_received_ && node_edge_map_received_ && current_pose_updated_)
+        {
+            ROS_INFO("========================================");
+            ROS_INFO("current_checkpoint_id: %d", current_checkpoint_id_);
+            ROS_INFO("next_checkpoint_id: %d", next_checkpoint_id_);
+            ROS_INFO("current_pose: (%f, %f)", current_pose_.pose.position.x, current_pose_.pose.position.y);
+            ROS_INFO("local_goal: (%f, %f)", local_goal_.pose.position.x, local_goal_.pose.position.y);
+            ROS_INFO("local_goal_index: %d", local_goal_index_);
+            ROS_INFO("local_goal_poses.size(): %d", (int)local_goal_poses_.size());
+
+            if(reached_goal(goal_node_, current_pose_) && checkpoint_.data.size() == 0)
+            {
+                ROS_INFO("reached_goal");
+                return;
+            }
+
+            if (local_goal_poses_.size() == 0)
+                get_node2node_poses(current_checkpoint_id_, next_checkpoint_id_, local_goal_poses_);
+
+            if (reached_checkpoint(next_checkpoint_id_, current_pose_))
+            {
+                ROS_WARN("reached_checkpoint");
+
+                // if (checkpoint_.data.size() == 0)
+                // {
+                //     ROS_WARN("Checkpoint is empty");
+                // }
+
+                local_goal_index_ = 0;
+                current_checkpoint_id_ = next_checkpoint_id_;
+                next_checkpoint_id_ = checkpoint_.data[0];
+                while (current_checkpoint_id_ == next_checkpoint_id_)
+                {
+                    checkpoint_.data.erase(checkpoint_.data.begin());
+                    next_checkpoint_id_ = checkpoint_.data[0];
+                }
+                get_node2node_poses(current_checkpoint_id_, next_checkpoint_id_, local_goal_poses_);
+                local_goal_ = get_local_goal(local_goal_poses_, local_goal_index_, current_pose_);
+
+                ROS_WARN("checkpoint updated");
+                ROS_WARN("current_checkpoint_id: %d", current_checkpoint_id_);
+                ROS_WARN("next_checkpoint_id: %d", next_checkpoint_id_);
+            }
+            else
+            {
+                local_goal_ = get_local_goal(local_goal_poses_, local_goal_index_, current_pose_);
+            }
+            local_goal_pub_.publish(local_goal_);
+            current_pose_updated_ = false;
+        }
+
         ros::spinOnce();
         rate.sleep();
     }
